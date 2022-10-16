96e0a0b708f55b1d4401c5cdf520dcf8ab4b9d08
igor.guariroba <igorguariroba@movida.com.br>
2022-10-16 17:38:23 -0300

    Dist

--- src/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components'
-import { defaultTheme } from '../styles/themes/default'
-
-type Themetype = typeof defaultTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends Themetype {}
-}
